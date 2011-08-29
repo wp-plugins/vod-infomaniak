d533446d92102de94ae20030efd0a9d1524606e1
infomaniak-dev <infomaniak-dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-29 08:53:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/vod-infomaniak/trunk@430033 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vod.php
+++ vod.php
@@ -4,7 +4,7 @@ Plugin Name: VOD Infomaniak
 Plugin URI: http://statslive.infomaniak.com
 Description: Easily embed and manage videos from Infomaniak VOD in your posts, comments and RSS feeds. You need an Infomaniak VOD account to use this plugin.
 Author: Destrem Kevin
-Version: 0.1.6
+Version: 0.1.7
 Author URI: http://infomaniak.com
 */
 
